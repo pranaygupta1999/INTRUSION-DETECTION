71e55d427957458f4a54b73d93484ceb0230fc7a
Pranay Gupta <pranaygupta1999@gmail.com>
2020-11-04 16:52:37 +0530

    Code reformatted

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow, ipcMain} = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 // var remote = require('remote'); // Load remote compnent that contains the dialog dependency
 // var dialog = remote.require('dialog'); // Load the dialogs component of the OS
 var fs = require('fs'); // Load the File System to execute our common tasks (CRUD)
@@ -13,7 +13,7 @@ ipcMain.on("result", (event, arg) => {
     // let reader = new FileReader;
 
     fs.readFile(filepath, 'utf-8', (err, data) => {
-        if(err){
+        if (err) {
             console.log("An error ocurred reading the file :" + err.message);
             return;
         }
@@ -21,7 +21,7 @@ ipcMain.on("result", (event, arg) => {
     });
 });
 // ipcMain.on("packets", (event, arg1) => {
- 
+
 //     // let reader = new FileReader;
 //     console.log(arg1);
 //     fs.readFile(filepath1, 'utf-8', (err, detect1) => {
@@ -43,7 +43,7 @@ ipcMain.on("result", (event, arg) => {
 //             console.log("An error ocurred reading the file :" + err.message);
 //             return;
 //         }
-        
+
 //         event.sender.send("persentage", detect2);
 //     });
 // })
@@ -54,17 +54,17 @@ ipcMain.on("run_script", (event, arg) => {
         console.log("scan is already running");
         return;
     }
-    var python = require('child_process').spawn('python', ['-u','./src/python/main.py']);
+    var python = require('child_process').spawn('python', ['-u', './src/python/main.py']);
     // var python = require('child_process').spawn('py', ['-u','./src/python/hello.py']); //Comment above line and uncomment this line for windows
-    python.stdout.on('data',function(data){
+    python.stdout.on('data', function (data) {
         isScanning = true;
         var outputLine = data.toString('utf8');
-        if( outputLine.split(' ')[0] == "UI-DATA" && outputLine.split(' ').length == 3)
+        if (outputLine.split(' ')[0] == "UI-DATA" && outputLine.split(' ').length == 3)
             event.sender.send("packets", outputLine.split(' ')[1], outputLine.split(' ')[2])
         process.stdout.write(outputLine);
-        
+
         // event.sender.send("render_again");
-            //packets info
+        //packets info
         // fs.readFile(filepath1, 'utf-8', (err, detect1) => {
         //     if(err){
         //         console.log("An error ocurred reading the file :" + err.message);
@@ -80,24 +80,24 @@ ipcMain.on("run_script", (event, arg) => {
         //         console.log("An error ocurred reading the file :" + err.message);
         //         return;
         //     }
-            
+
         //     event.sender.send("persentage", detect2);
         // });
     });
 
 });
 
-function createWindow(){
+function createWindow() {
     win = new BrowserWindow({
-        width:800, 
+        width: 800,
         height: 520,
         frame: false,
         webPreferences: {
-        nodeIntegration: true
-    }
-    
+            nodeIntegration: true
+        }
+
     })
-    
+
     win.loadURL('http://localhost:3000/home')
 }
 app.on('ready', createWindow)
@@ -106,6 +106,6 @@ app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-      app.quit()
+        app.quit()
     }
-  })
+})
